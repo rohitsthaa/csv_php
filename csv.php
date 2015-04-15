afc37be6fce47274e1cc4b528194ae0620360bb9
Roht Shrestha <sth.rohit@gmail.com>
2015-04-15 04:31:26 -0400

    imports csv to database

---
+++ csv.php
@@ -0,0 +1,45 @@
+
+<body>
+<div id="container">
+<div id="form">
+
+<?php
+
+$host='localhost';
+    $uname='root';
+    $pwd='heavenhell';
+    $db="csv";
+
+    $con = mysql_connect($host,$uname,$pwd) or die("connection failed");
+    mysql_select_db($db,$con) or die("db selection failed");
+$name= 'discounts.csv';
+$deleterecords = "TRUNCATE TABLE discount"; //empty the table of its current records
+mysql_query($deleterecords);
+
+
+        readfile($name);
+    //Import uploaded file to Database
+    $handle = fopen($name, "r");
+
+   
+    $i=0;
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+    if($i>0){
+        $import="INSERT into discount(id,title,expired_date,amount)values('".$data[0]."','".$data[1]."','".$data[2]."','".$data[3]."')";
+        mysql_query($import) or die(mysql_error());
+    }
+    $i=1;
+    }
+
+    fclose($handle);
+
+    print "Import done";
+
+
+?>
+
+<?php
+
+
+
+?>
